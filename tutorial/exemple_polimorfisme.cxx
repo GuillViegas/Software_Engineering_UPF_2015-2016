0a9dd467b4c8850faba87693b0673ae1949677a4
Guilherme Viegas <guilherme.viegas01@estudiant.upf.edu>
2016-01-31 21:35:11 +0100

    Second step

--- tutorial/exemple_polimorfisme.cxx
+++ tutorial/exemple_polimorfisme.cxx
@@ -1,11 +1,16 @@
+/* 
+  - definició d'una classe
+  - instaciacio i missatge a un objecte
+ */
+#include <iostream>
+
 class Animal
 {
-public:
 	void escriuEspecie()
 	{
 		std::cout << "...indefinida..." << std::endl;
 	}
-};
+}
 
 int main(void)
 {
