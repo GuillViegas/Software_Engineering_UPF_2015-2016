594def5663429b9972aea64b1c2e133e1785b5a6
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-02-01 11:43:35 +0100

    Third tutorial completed

---
+++ doc/practica/tutorial/03_exemple_polimorfisme.cxx
@@ -0,0 +1,22 @@
+/* 
+  - classe acabada en ;
+  - mètode public:
+ */
+#include <iostream>
+
+class Animal
+{
+public:
+	void escriuEspecie()
+	{
+		std::cout << "...indefinida..." << std::endl;
+	}
+};
+
+int main(void)
+{
+	Animal unAnimal;
+	unAnimal.escriuEspecie();
+
+	return 0;
+}
