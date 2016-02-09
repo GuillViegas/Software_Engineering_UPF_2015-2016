61af29956c826b20980d3743206840d1c64029af
Guilherme Viegas <guilherme.viegas01@estudiant.upf.edu>
2016-02-09 15:20:58 +0100

    RED: ItemTests::testTitle_byDefault

--- src/albuns.hxx
+++ src/albuns.hxx
@@ -4,10 +4,27 @@
 #include <string>
 
 class Album{
+/*protected:
+	std::string t;
+
+public:
+	Album (){
+		t = "-- Untitled --";
+	}
+
+*/
 public:
 	std::string title(){
-		return "_test_";
+		return "";
+		//return t;
 	}
+
+/*
+public:
+	void title(std::string _title){
+		//t = _title;
+	}
+*/
 };
 
 #endif
