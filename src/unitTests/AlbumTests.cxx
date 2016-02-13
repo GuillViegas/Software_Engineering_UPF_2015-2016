d08c37847dcc88980d547005ca6248682226e021
Guilherme Viegas <guilherme.viegas01@estudiant.upf.edu>
2016-02-13 18:49:53 +0100

    uncommented testTitle_afterModifying

--- src/unitTests/AlbumTests.cxx
+++ src/unitTests/AlbumTests.cxx
@@ -7,7 +7,7 @@ public:
 	TEST_FIXTURE( AlbumTests )
 	{
 		TEST_CASE( testTitle_byDefault );
-		//TEST_CASE( testTitle_afterModifying );
+		TEST_CASE( testTitle_afterModifying );
 		//TEST_CASE( testListed_byDefault );
 		//TEST_CASE( testListed_afterListing );
 		//TEST_CASE( testListed_afterUnlisting );
@@ -19,7 +19,7 @@ public:
 		ASSERT_EQUALS( "-- Untitled --", album.title() );
 	}
 
-	/*
+	
 	void testTitle_afterModifying()
 	{
 		Album album;
@@ -27,7 +27,7 @@ public:
 		ASSERT_EQUALS( "A title", album.title() );
 	}
 	
-	
+	/*
 	void testListed_byDefault()
 	{
 		Album album;
