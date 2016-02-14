91fa7d642697e0fe3d77054849b3e950c5cab377
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-02-14 22:21:15 +0100

    New class Artist

---
+++ src/Artist.hxx
@@ -0,0 +1,30 @@
+#ifndef artist_hxx
+#define artist_hxx
+
+#include <iostream>
+#include <string>
+
+class Artist {
+
+public:
+
+	Artist() {
+	}
+
+	/*
+	std::string getName() {
+		return t;
+	}*/
+
+	/*
+	std::string getInfo() {
+		return
+	}
+	*/
+
+private:
+	std::string name;
+
+};
+
+#endif
