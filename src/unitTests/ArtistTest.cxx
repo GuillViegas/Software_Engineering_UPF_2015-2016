3fe9506844f3dad5a789af6716d0d7f9140f501b
Guilherme Viegas <guilherme.viegas01@estudiant.upf.edu>
2016-02-15 10:25:23 +0100

    ArtistTest.cxx and Artist.hxx commented code

---
+++ src/unitTests/ArtistTest.cxx
@@ -0,0 +1,59 @@
+#include "MiniCppUnit.hxx"
+#include "Artist.hxx"
+
+class ArtistTests : public TestFixture<ArtistTests>
+{
+public:
+	TEST_FIXTURE( ArtistTests )
+	{
+		//TEST_CASE( testName_byDefault );
+		//TEST_CASE( testName_afterModifying );
+		//TEST_CASE( testGroup_byDefault );
+		//TEST_CASE( testGroup_afterModifying );
+		//TEST_CASE( testSolo_byDefault );
+		//TEST_CASE( testSolo_afterModifying );
+	}
+
+	/*
+	void testName_byDefault()
+	{
+		Artist artist;
+		ASSERT_EQUALS( "-- Unnamed --", artist.title() );
+	}
+
+	void testName_afterModifying()
+	{
+		Artist artist;
+		artist.name( "A Name" );
+		ASSERT_EQUALS( "A Name", track.name() );
+	}
+	
+	void testGroup_byDefault()
+	{
+		Artist artist;
+		ASSERT_EQUALS( false , track.isGroup() );
+	}
+
+	void testGroup_afterModifying()
+	{
+		Artist artist;
+		artist.group();
+		ASSERT_EQUALS( true , track.isGroup() );
+	}
+
+	void testSolo_byDefault()
+	{
+		Artist artist;
+		ASSERT_EQUALS( false , track.isSolo() );
+	}
+
+		void testSolo_afterModifying()
+	{
+		Artist artist;
+		artist.solo();
+		ASSERT_EQUALS( true , track.isSolo() );
+	}
+	*/
+
+
+};
