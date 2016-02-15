5114174902b5200dd0fc0d465e0e92f1418b9046
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-02-15 14:04:44 +0100

    File changed

--- src/unitTests/ArtistTest.cxx
+++ src/unitTests/ArtistTest.cxx
@@ -1,12 +1,12 @@
 #include "MiniCppUnit.hxx"
 #include "Artist.hxx"
 
-class ArtistTests : public TestFixture<ArtistTests>
+class ArtistTest : public TestFixture<ArtistTest>
 {
 public:
-	TEST_FIXTURE( ArtistTests )
+	TEST_FIXTURE( ArtistTest )
 	{
-		//TEST_CASE( testName_byDefault );
+		TEST_CASE( testName_byDefault );
 		//TEST_CASE( testName_afterModifying );
 		//TEST_CASE( testGroup_byDefault );
 		//TEST_CASE( testGroup_afterModifying );
@@ -14,31 +14,32 @@ public:
 		//TEST_CASE( testSolo_afterModifying );
 	}
 
-	/*
+	
 	void testName_byDefault()
 	{
 		Artist artist;
-		ASSERT_EQUALS( "-- Unnamed --", artist.title() );
+		ASSERT_EQUALS( "-- No name --", artist.name() );
 	}
 
+	/*
 	void testName_afterModifying()
 	{
 		Artist artist;
 		artist.name( "A Name" );
-		ASSERT_EQUALS( "A Name", track.name() );
+		ASSERT_EQUALS( "A Name", artist.name() );
 	}
 	
 	void testGroup_byDefault()
 	{
 		Artist artist;
-		ASSERT_EQUALS( false , track.isGroup() );
+		ASSERT_EQUALS( false , artist.isGroup() );
 	}
 
 	void testGroup_afterModifying()
 	{
 		Artist artist;
 		artist.group();
-		ASSERT_EQUALS( true , track.isGroup() );
+		ASSERT_EQUALS( true , artist.isGroup() );
 	}
 
 	void testSolo_byDefault()
