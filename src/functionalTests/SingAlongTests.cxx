f480cd387d548508fc4eacbf35a0998700d1e532
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-04-17 23:36:16 +0200

    RED:SingAlongTests::testAddTrack_whenMasterDoesNotExist

--- src/functionalTests/SingAlongTests.cxx
+++ src/functionalTests/SingAlongTests.cxx
@@ -18,7 +18,7 @@ public:
 		TEST_CASE( testCatalog_withTwoTracksWithDifferentDuration );
 		TEST_CASE( testCatalog_withTracksOfDifferentArtists );
 		TEST_CASE( testAddTrack_toArtistThatDoesNotExist );
-		//TEST_CASE( testAddTrack_whenMasterDoesNotExist );
+		TEST_CASE( testAddTrack_whenMasterDoesNotExist );
 		//TEST_CASE( testCatalog_withUnlistedAlbum );
 		//TEST_CASE( testCatalog_withListedAlbum );
 		//TEST_CASE( testCatalog_withAlbumListedAndUnlisted );
@@ -190,7 +190,7 @@ public:
 		{
 			ASSERT_EQUALS( "The artist does not exist", e.what() );
 		}
-	}/*
+	}
 	void testAddTrack_whenMasterDoesNotExist()
 	{
 		SingAlong business;
@@ -204,7 +204,7 @@ public:
 		{
 			ASSERT_EQUALS( "The master file does not exist", e.what() );
 		}
-	}
+	}/*
 	void testCatalog_withUnlistedAlbum()
 	{
 		SingAlong business;
