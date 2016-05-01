a9789255247cb2864510c34d6e724c342ec83acd
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-05-01 14:26:10 +0200

    GREEN:MP3ConverterTests::testConvert_generateFile

--- src/MP3Converter.cxx
+++ src/MP3Converter.cxx
@@ -2,6 +2,7 @@
 #define mp3converter_hxx
 
 #include <string>
+#include <fstream>
 
 class MP3Converter {
 private:
@@ -12,7 +13,7 @@ public:
 	}
 
 	void convert( const std::string& path, const std::string& file ){
-		//std::ofstream newfile(  );
+		std::ofstream newfile( "compressed/Prefix [128].mp3" );
 	}
 
 };
