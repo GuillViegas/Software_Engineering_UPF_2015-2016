d5b3900f73ef1113b75dd08695a67b3e9c152665
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-05-01 23:52:53 +0200

    RED:OggConverterTests::testConvert_generateFile

---
+++ src/OggConverter.cxx
@@ -0,0 +1,41 @@
+#ifndef mp3converter_hxx
+#define mp3converter_hxx
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include "Converter.cxx"
+
+
+class directoryException : public std::exception {
+public:
+	const char * what() const throw() {
+		return "The master file does not exist";
+	}
+};
+
+class OggConverter {
+private:
+public:
+	OggConverter() {}
+	~OggConverter() {}
+
+	void convert( const std::string& path, const std::string& file ) {
+
+		//Check if path exists
+		std::ifstream my_file(path.c_str());
+
+		if ( my_file.good() ) {
+			std::string outputFile( file );
+			outputFile += " [128].mp3";
+			std::ofstream newfile( outputFile.c_str() );
+		} else throw directoryException();
+	}
+
+};
+
+
+#endif
---
+++ src/unitTests/OggConverterTests.cxx
@@ -0,0 +1,130 @@
+#include "MiniCppUnit.hxx"
+#include "LibFileSystem.hxx"
+#include <fstream>
+
+#include "OggConverter.cxx"
+//#include "Converter.cxx"
+
+class OggConverterTests : public TestFixture<OggConverterTests>
+{
+public:
+	TEST_FIXTURE ( OggConverterTests )
+	{
+		TEST_CASE( testConvert_generateFile );
+		//TEST_CASE( testConvert_generateContent );
+		//TEST_CASE( testConvert_withDifferentBitrate );
+		//TEST_CASE( testConvert_withUnsupportedFormat );
+		//TEST_CASE( testConvert_withInexistentMaster );
+		//TEST_CASE( testConvert_polymorphicCall );
+
+	}
+
+	/**
+	 * setUp is automatically called before each test case
+	 */
+	void setUp()
+	{
+		LibFileSystem::createNetDirectory( "masters" );
+		LibFileSystem::createNetDirectory( "compressed" );
+		LibFileSystem::createNetDirectory( "config" );
+	}
+
+	/**
+	 * tearDown is automatically called after each test case
+	 */
+	void tearDown()
+	{
+		LibFileSystem::cleanupDirectory( "masters" );
+		LibFileSystem::cleanupDirectory( "compressed" );
+		LibFileSystem::cleanupDirectory( "config" );
+	}
+
+	void createMasterFile( const std::string & name, int duration )
+	{
+		std::string completeName( "masters/" );
+		completeName += name;
+		std::ofstream os( completeName.c_str() );
+		os << duration << std::endl;
+	}
+	
+	
+	void testConvert_generateFile() 
+	{
+		OggConverter converter;
+
+		createMasterFile( "Master.wav", 50);
+		converter.convert( "masters/Master.wav", "compressed/Prefix" );
+
+		ASSERT_EQUALS(
+			"compressed/Prefix [128].ogg\n",
+			LibFileSystem::listDirectoryInOrder( "compressed" )
+		);
+
+	}
+	/*
+	void testConvert_generateContent() 
+	{
+
+		OggConverter converter;
+
+		createMasterFile( "Master.wav", 50);
+		converter.convert( "masters/Master.wav", "compressed/Prefix" );
+		converter.addContentToFile("compressed/Prefix", "MP3 extracted from 'masters/Master.wav' at 128 bps and length 50s.");
+
+		ASSERT_EQUALS(
+			"MP3 extracted from 'masters/Master.wav' at 128 bps and length 50s.\n",
+			LibFileSystem::fileContent( "compressed/Prefix [128].mp3" )
+		);
+	}
+
+	
+	void testConvert_withDifferentBitrate() 
+	{
+		OggConverter converter;
+
+		createMasterFile( "Master.wav", 50);
+		converter.convert( "masters/Master.wav", "compressed/Prefix" );
+		converter.bitRate(96);
+
+		ASSERT_EQUALS(
+			"compressed/Prefix [96].mp3\n",
+			LibFileSystem::listDirectoryInOrder( "compressed" )
+		);
+
+	}
+	
+	void testConvert_withInexistentMaster() 
+	{
+		OggConverter converter;
+	try 
+	{
+		converter.convert( "master/Master.wav", "compressed/Prefix" );
+		FAIL( "An exception should be caught!" );
+	}
+	catch ( std::exception & e)
+	{
+		ASSERT_EQUALS( "The master file does not exist", e.what() );
+	}
+
+	}
+
+
+	void testConvert_polymorphicCall() 
+	{
+		OggConverter oggconverter;
+		Converter & converter = mp3converter;
+
+		createMasterFile( "Master.wav", 50);
+		converter.convert( "masters/Master.wav", "compressed/Prefix" );
+
+		ASSERT_EQUALS(
+			"compressed/Prefix [128].mp3\n",
+			LibFileSystem::listDirectoryInOrder( "compressed" )
+		);
+	}
+	*/
+
+
+};
+
+REGISTER_FIXTURE( OggConverterTests )
