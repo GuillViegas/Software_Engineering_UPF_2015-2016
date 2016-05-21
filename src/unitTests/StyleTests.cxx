b5f6d670501628d866a524023ed5f336c4bac9fd
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-05-21 20:46:05 +0200

    RED:StyleTests::testStyle_createNewStyle

---
+++ src/unitTests/StyleTests.cxx
@@ -0,0 +1,23 @@
+#include "MiniCppUnit.hxx"
+#include "Style.hxx"
+
+class StyleTests : public TestFixture<StyleTests>
+{
+public:
+	TEST_FIXTURE( StyleTests) {
+		TEST_CASE( testStyle_createNewStyle );
+		// TEST_CASE( testStyle_addUsertoSubscribersList );
+	}
+
+	void testStyle_createNewStyle() 
+	{
+		Style style(" house ");
+		ASSERT_EQUALS(
+			"house", 
+			style.getNameStyle()
+		);
+	}
+
+};
+
+REGISTER_FIXTURE( StyleTests) 
