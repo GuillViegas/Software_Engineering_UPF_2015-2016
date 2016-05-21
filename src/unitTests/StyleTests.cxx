ad7b2a15c33e8e4c8695ec173861f7e5375e380f
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-05-21 21:00:57 +0200

    RED:StyleTests::testStyle_addUsertoSubscribersList

--- src/unitTests/StyleTests.cxx
+++ src/unitTests/StyleTests.cxx
@@ -6,7 +6,7 @@ class StyleTests : public TestFixture<StyleTests>
 public:
 	TEST_FIXTURE( StyleTests) {
 		TEST_CASE( testStyle_createNewStyle );
-		// TEST_CASE( testStyle_addUsertoSubscribersList );
+		TEST_CASE( testStyle_addUsertoSubscribersList );
 	}
 
 	void testStyle_createNewStyle() 
@@ -16,6 +16,18 @@ public:
 			"house", 
 			style.getNameStyle()
 		);
+	}
+
+	void testStyle_addUsertoSubscribersList() 
+	{
+		Style style("classic");
+		User user("elena", "elena@gmail.com");
+		style.addUsertoSubscribers(user);
+		ASSERT_EQUALS(
+			"elena", 
+			style.subscribersList()
+		);
+
 	}
 
 };
