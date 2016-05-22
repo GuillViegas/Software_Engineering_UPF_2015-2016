1c9a8898d340d1d02136c97c50c2f8f2f723b984
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-05-22 13:52:47 +0200

    Cleaned code. Improved code in ConverterGroup class

--- src/ConverterGroup.hxx
+++ src/ConverterGroup.hxx
@@ -8,6 +8,7 @@
 #include "Converter.hxx"
 #include "MP3Converter.hxx"
 #include "OggConverter.hxx"
+#include "Exception.hxx"
 
 
 class ConverterGroup {
@@ -27,22 +28,14 @@ public:
 				
 				MP3Converter mp3converter;
 				mp3converter.convert("masters/Master.wav","compressed/Prefix");
-
-				if (rate == 96) mp3converter.bitRate(96);
-				else if (rate == 128) mp3converter.bitRate(128);
-				else mp3converter.bitRate(192);
-
+				mp3converter.bitRate(rate);
 				converterlist.push_back(mp3converter);
 
 			} else if (path == "ogg") {
 
 				OggConverter oggconverter;
 				oggconverter.convert("masters/Master.wav","compressed/Prefix");
-
-				if (rate == 96) oggconverter.bitRate(96);
-				else if (rate == 128) oggconverter.bitRate(128);
-				else oggconverter.bitRate(192);
-
+				oggconverter.bitRate(rate);
 				converterlist.push_back(oggconverter);
 			
 			} else throw ConverterException();
