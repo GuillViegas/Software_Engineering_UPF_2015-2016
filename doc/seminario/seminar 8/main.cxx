d09992cdee238a74f7beafa5734d0773be905432
Juan Costa <juanantonio.costa01@estudiant.upf.edu>
2016-05-31 23:00:30 +0200

    Seminar 9

--- doc/seminario/seminar 8/main.cxx
+++ doc/seminario/seminar 8/main.cxx
@@ -5,21 +5,19 @@
 int main() {
 	
 	//Sum test
-	Fraction<int> fraction1(5, 2);
-	Fraction<int> fraction2(2, 5);
+	Fraction<int> fraction1(2, 2);
+	Fraction<int> fraction2(2, 2);
 	
 	Fraction<int> fraction3 = fraction1+fraction2;
 	
+	std::cout << "The result of the sum of fractions is: " << fraction3.valueNum() << "/" << fraction3.valueDen() <<  std::endl;
 	
 	//Multiplication test
-	Fraction<int> fraction4(5, 2);
-	Fraction<int> fraction5(2, 5);
+	Fraction<int> fraction4(100, 3);
+	Fraction<int> fraction5(3, 100);
 	
 	Fraction<int> fraction6 = fraction4*fraction5;
 		
-	std::cout << fraction3.valueNum() << " / " << fraction3.valueDen() <<  std::endl;
-	std::cout << fraction6.valueNum() << " / " << fraction6.valueDen() << std::endl;
-	
-	
+	std::cout << "The result of the multiplication of fractions is: " << fraction6.valueNum() << "/" << fraction6.valueDen() << std::endl;
 	
 }
